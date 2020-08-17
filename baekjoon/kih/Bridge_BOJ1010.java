fd31655b2f9c508f0cdc5b5b7432b526ae1e9a98
ilhwan <kih3159@naver.com>
2020-08-17 14:17:24 +0900

    좌표정렬

--- baekjoon/kih/Bridge_BOJ1010.java
+++ baekjoon/kih/Bridge_BOJ1010.java
@@ -1,8 +1,10 @@
+package baekjoon.kih;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Bridge_BOJ1010 {
     public static void main(String[] args) throws IOException {
 
         int[][] d = new int[31][31];
--- baekjoon/kih/SugarDelivery_BOJ2839.java
+++ baekjoon/kih/SugarDelivery_BOJ2839.java
@@ -1,4 +1,4 @@
-package Trash;
+package baekjoon.kih;
 
 import java.io.BufferedReader;
 import java.io.IOException;
