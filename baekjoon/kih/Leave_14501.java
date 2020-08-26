4cb918b4cb9146895b574c715da8835244ec84d4
김일환 <ilhwan@stunitas.com>
2020-08-26 10:17:39 +0900

    퇴사

--- baekjoon/kih/Leave_14501.java
+++ baekjoon/kih/Leave_14501.java
@@ -5,15 +5,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Leave_14501 {
+
+    public static int n;
+    public static int[] t;
+    public static int[] p;
+    public static int answer = 0;
+
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-
-        int[] t = new int[n + 1];
-        int[] p = new int[n + 1];
-        int[] d = new int[n+1];
+        n = Integer.parseInt(br.readLine());
+        t = new int[n + 1];
+        p = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
 
@@ -25,13 +29,26 @@ public class Leave_14501 {
 
         /*
          * d[n] : n일 까지 상담했을 때 최대 수익
-         * 1) : n일에 상담 했을 경우 (단 t[n] == 1)
-         *      d[n] = p[n] + d[n-1]
-         * 2) : n일에 상담하지 않았을 경우
-         *      d[n] = d[n-1]
+         * case 1) : n일에 상담 했을 경우
+         *          d[n] = p[n] + d[n-1]
+         * case 2) : n일에 상담하지 않았을 경우
+         *          d[n] = d[n-1]
          */
 
+        solution(1, 0);
 
+        System.out.println(answer);
+    }
 
+    private static void solution(int day, int sum) {
+
+        if (day > n + 1) return;
+        if (day == n + 1)  answer = Math.max(answer, sum);
+
+        // day 날짜에 상담 했을 경우
+        solution(day + t[day], sum + p[day]);
+
+        // day 날짜에 상담하지 않았을 경우
+        solution(day + 1, sum);
     }
 }
