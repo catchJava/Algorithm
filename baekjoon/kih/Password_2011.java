522f8ba35e46291508aca5f060ebb37018512f96
김일환 <ilhwan@stunitas.com>
2020-10-13 10:08:44 +0900

    암호찾기

--- baekjoon/kih/Password_2011.java
+++ baekjoon/kih/Password_2011.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 
 public class Password_2011 {
 
-    private static final int MOD = 1;
+    private static final int MOD = 1000000;
 
     public static void main(String[] args) throws Exception {
 
@@ -13,20 +13,20 @@ public class Password_2011 {
 
         String password = br.readLine();
 
-        dp(password);
+        System.out.println(dp(password));
     }
 
-    private static void dp(String password) {
+    private static long dp(String password) {
 
         int len = password.length();
 
         if (len == 1) {
-            System.out.println(1);
-            return;
+            return 1;
         }
 
-        long[] d = new long[len];
-        d[1] = 1;
+        long[][] d = new long[len][2];
+        d[0][0] = 0; // 쪼개졌을 때 ex) 1.1
+        d[0][1] = 1; // 붙어있을 때 ex) 11
 
         for (int i = 1; i < len; i++) {
 
@@ -34,12 +34,12 @@ public class Password_2011 {
             int cur_num = password.charAt(i) - '0';
 
             if ((pre_num * 10) + cur_num > 26) {
-                d[i] = d[i - 1];
+                // d[i] = d[i - 1] % MOD;
             } else {
-                d[i] = d[i - 1] + 1;
+                // d[i] = (d[i - 1] + 1) % MOD;
             }
         }
 
-        System.out.println(d[len - 1]);
+        return d[len - 1][0] + d[len - 1][2];
     }
 }
